a31d560c61ce7ed18957bd25fcd2099d3268bacf
songjiubin <songjiubin@gmail.com>
2018-10-03 20:34:55 +0800

    fix the package name

--- library/src/androidTest/java/com/github/songjiubin/pageindexer/ExampleInstrumentedTest.kt
+++ library/src/androidTest/java/com/github/songjiubin/pageindexer/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.github.songjiubin.pageindexer.demo", appContext.packageName)
+        assertEquals("com.github.songjiubin.pageindexer", appContext.packageName)
     }
 }
